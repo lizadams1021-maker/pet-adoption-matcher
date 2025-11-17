1772b12492a8b109e8a88be10e2a6683dc407176
kevin <kevin.fernandez@ntsprint.com>
2025-11-17 14:07:42 -0500

    Completly remove the dev bubble.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
-  devIndicators: {
-    buildActivity: false
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  devIndicators: false, // ✅ this hides the dev bubble
 };
 
 export default nextConfig;
